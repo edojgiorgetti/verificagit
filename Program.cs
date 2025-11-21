bdc94a35731b908b601cc55b4cb470d2bbd0a31a
Edoardo Giorgetti <EDOARDO.GIORGETTI@local.ittcesena.it>
2025-11-21 09:22:32 +0100

    R0.2: completato parzialmente il program.cs

--- Program.cs
+++ Program.cs
@@ -9,6 +9,27 @@
 
             int scelta;
             int scelta2;
+            do
+            {
+                Console.WriteLine("Se vuoi aggiungere un libro alla biblioteca clicca 1, se vuoi cercare un libro per titolo digita 2, se vuoi cercare i libri di un autore digita 3, se vuoi contare il numero di libri nella biblioteca digita 4 ");
+                scelta2 = int.Parse(Console.ReadLine());
+                switch (scelta2)
+                {
+                    case 1:
+                    //aggiunge il libro
+                    case 2:
+                    //richiamo il metodo per cercare il libro per titolo
+                    case 3:
+                    //richiamo il metoto per trovare i libri di un autore
+                    case 4:
+                        //richiamo il metodo che restituisce il numero di libri della biblioteca
+
+                }
+
+                // chiedo all'utente se vuole continuare a gestire la sua libreria o vuole fermarsi
+                Console.WriteLine("se vuoi continuare digita 1 altrimenti 0");
+                scelta = int.Parse(Console.ReadLine());
+            } while (scelta == 1);
         }
     }
 }
