5ac20ee352f044c8210400ad310320249a8bbcff
edojgiorgetti <giorgetti.edoardo3@gmail.com>
2025-11-21 09:32:28 +0100

    Add comment for pull request context

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // commento per fare il pull
             Libro libro1 = new Libro("Manzoni", "I promessi sposi", "zanichelli", new DateTime(1900, 10, 08), 800);
             Libro libro2 = new Libro("Boccaccio", "La bella addormentata nel bosco", "zanichelli", new DateTime(1901, 11, 12), 500);
 
