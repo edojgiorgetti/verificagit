ade9bb9e76036cfbdf1332c2bde7feea51884d44
Edoardo Giorgetti <EDOARDO.GIORGETTI@local.ittcesena.it>
2025-11-21 09:20:28 +0100

    R0.1: iniziato il program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Libro libro1 = new Libro("Manzoni", "I promessi sposi", "zanichelli", new DateTime(1900, 10, 08), 800);
+            Libro libro2 = new Libro("Boccaccio", "La bella addormentata nel bosco", "zanichelli", new DateTime(1901, 11, 12), 500);
+
+            int scelta;
+            int scelta2;
         }
     }
 }
