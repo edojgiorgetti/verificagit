b3c6d82aa40d91347290c5866f6c97647779ce1b
Edoardo Giorgetti <EDOARDO.GIORGETTI@local.ittcesena.it>
2025-11-21 08:58:56 +0100

    R0.2: completata la classe Bibblioteca

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -22,5 +22,53 @@ namespace verificagit
             OrarioChiusura = orarioChiusura;
             Libri = libri;
         }
+
+        public void AggiungiLibro(Libro libro)
+        {
+            Libri.Add(libro);
+        }
+
+        public List<Libro> CercaTitolo(string titolo)
+        {
+            List<Libro> libritrovati = new List<Libro>();
+            foreach (Libro libro in Libri)
+            {
+                if (libro.Titolo.Contains(titolo))
+                {
+                    libritrovati.Add(libro);
+                }
+            }
+            if (libritrovati.Count == 0)
+            {
+                Console.WriteLine("mi dispiace non abbiamo trovato nessun libro col titolo inserito");
+            }
+
+            return libritrovati;
+        }
+
+        public List<Libro> CercaAutore(string autore)
+        {
+            List<Libro> autoritrovati = new List<Libro>();
+            foreach (Libro libro in Libri)
+            {
+                if (libro.Autore.Contains(autore))
+                {
+                    autoritrovati.Add(libro);
+                }
+            }
+            if (autoritrovati.Count == 0)
+            {
+                Console.WriteLine("nessun autore è stato trovato");
+            }
+
+            return autoritrovati;
+        }
+
+        public int NumLibri()
+        {
+            int num;
+            num = Libri.Count();
+            return num;
+        }
     }
 }
